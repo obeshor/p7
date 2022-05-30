3121d06939f834fd1d8393f9f532ccfff4f960c2
Ibtissam Radi <chamseddine.ibtissam@gmail.com>
2022-05-30 23:15:16 +0200

    création dashboard

---
+++ dashboard.py
@@ -0,0 +1,11 @@
+import requests
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import plotly.express as px
+import numpy as np
+
+plt.style.use('fivethirtyeight')
+sns.set_style('darkgrid')
