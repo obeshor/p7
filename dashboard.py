4e7f3ee87df31c19455e78dead45d5a0d7f60397
Ibtissam Radi <chamseddine.ibtissam@gmail.com>
2022-05-30 23:17:55 +0200

    mise à jour dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,3 +9,11 @@ import numpy as np
 
 plt.style.use('fivethirtyeight')
 sns.set_style('darkgrid')
+#Title display
+html_temp = """
+<div style="background-color: black; padding:10px; border-radius:10px">
+<h1 style="color: white; text-align:center">Dashboard Scoring Credit</h1>
+</div>
+<p style="font-size: 20px; font-weight: bold; text-align:center">Prédictions de scoring client et comparaison à l'ensemble des clients </p>
+"""
+st.markdown(html_temp, unsafe_allow_html=True)
